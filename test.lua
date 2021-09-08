3b5ecda45fe7d543b93d6e48d4dd4525d6b095dd
Rakaneth <rakaneth@gmail.com>
2021-09-08 12:49:03 -0400

    removing dll

--- test.lua
+++
@@ -1,9 +0,0 @@
-local Entity = require 'entity'
-local utils = require 'utils'
-local terminal = require 'BearLibTerminal'
-local templates = require 'templates'
-local ROT = require 'lib.rotLove.src.rot'
-
-local player = Entity(templates.player)
-
-print(player.name)
